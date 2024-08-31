75dc836d74be500c0137f4c37ff79a1bc2a2f767
adityapratamaf <adityapeef18@gmail.com>
2024-08-31 16:32:26 +0700

    Checkout Menu Update 2

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,12 @@ const {
 } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
